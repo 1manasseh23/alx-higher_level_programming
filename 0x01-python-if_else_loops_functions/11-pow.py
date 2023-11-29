74f2e50a73963f9dad047b5c4787b56c12556183
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-29 15:57:26 -0800

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    output = 1
-    if b < 0:
-        a = 1 / a
-        b = abs(b)
-
-    for _ in range(b):
-        output *= a
-
-    return output
+    return (a ** b)
