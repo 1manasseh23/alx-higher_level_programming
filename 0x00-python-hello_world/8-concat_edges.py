a3febfb3f5b08e19d606a40145687254cb3016f3
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-29 02:40:04 -0800

    creat new

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-object_oriented, programming, with_python = str[41:58], str[59:70], str[81:93]
-new_string = object_oriented + " " + programming +" " + with_python
-print(new_string)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[-23:-17] + str[0:6]
+print(str)
