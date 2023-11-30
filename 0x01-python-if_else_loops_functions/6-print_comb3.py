082ce9e527601dde8de404bf6b0e07c523898f2b
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-29 16:12:00 -0800

    Inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
-for cleta in range(10):
-    for vivan in range(cleta + 1, 10):
-        print("{:d}{:d}".format(cleta, vivan), end=", " if cleta != 8\
-                or vivan != 9 else "\n")
+for i in range(10):
+    # Start the inner loop from i + 1
+    for j in range(i + 1, 10):
+        if j == 9 and i == 8:
+            print("{}{}".format(i, j))
+            break
+        else:
+            print("{}{},".format(i, j), end=' ')
