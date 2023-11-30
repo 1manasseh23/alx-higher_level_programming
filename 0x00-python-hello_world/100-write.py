2ac961e3201df72848c6f6348bc646ea944f19c3
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-29 16:51:43 -0800

    Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-
-prt_str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(prt_str)
+massage = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(massage)
 sys.exit(1)
