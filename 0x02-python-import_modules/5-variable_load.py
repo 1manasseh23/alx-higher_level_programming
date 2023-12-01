b80eb175d4b6db7532a8539ca7d98392acac302e
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-30 19:21:44 -0800

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
-import importlib.util
-import sys
-
-def load_variable_value(file_path):
-    spec = importlib.util.spec_from_file_location("variable_load_5", file_path)
-
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    print(module.a)
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <file_path>".format(sys.argv[0]))
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    load_variable_value(file_path)
+    print(a)
