34ecca083883e7b45b00a8ddee38842843ecac65
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-12-03 15:29:41 -0800

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for glk in my_string:
-        if glk != 'c' and glk != 'C':
+        if glk != "c" and glk != "C":
             new_str = new_str + glk
     return(new_str)
