9308162a072f7549a94c8275abe804c31b4789aa
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-12-04 00:13:07 -0800

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,5 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    my_tuple1 = tuple_a + (0, 0)
+    my_tuple2 = tuple_b + (0, 0)
+    val = (my_tuple1[0] + my_tuple2[0], my_tuple1[1] + my_tuple2[1])
+    return(val)
