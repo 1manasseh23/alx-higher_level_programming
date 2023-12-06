6b1a5a72811c39a951f72c24acc6abf01b1ccff5
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-12-06 10:45:03 -0800

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     cl = a_dictionary
     cl.update({key: value})
     return cl
-    #a_dictionary[key] = value
-    #return a_dictionary
+    # a_dictionary[key] = value
+    # return a_dictionary
