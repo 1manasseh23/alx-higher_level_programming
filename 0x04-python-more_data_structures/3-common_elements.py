411b1feba442151c9a9bcfa8fff30aee45bc6095
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-12-06 07:58:39 -0800

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,4 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
     print_set = set_1.intersection(set_2)
-    """
-    set()
-    for setter in set_1:
-        if setter in set_2:
-            print_set.add(setter)
-            """
     return print_set
