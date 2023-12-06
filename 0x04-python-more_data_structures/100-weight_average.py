83cc77ea2e9f91aa9133c0e1a11b15a7707a6f89
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-12-06 12:26:42 -0800

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,5 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    return sum(me * love for me, love in my_list)
-    / sum(love for _, love in my_list)
+    return sum(me * love for me,
+               love in my_list) / sum(love for _, love in my_list)
