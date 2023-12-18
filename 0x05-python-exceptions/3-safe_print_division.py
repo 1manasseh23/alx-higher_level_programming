4e807105608e0a0612001244e03810bafb5869a0
1manasseh23 <cletavivan@gmail.com>
2023-12-18 09:07:46 -0800

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        output = a / b
+    except ZeroDivisionError:
+        output = None
+    finally:
+        print("Inside output: {}".format(output))
+        return output
