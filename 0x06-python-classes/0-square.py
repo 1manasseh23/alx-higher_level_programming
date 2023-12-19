7970ed06fd09dc05d63f302e37187e2d6bec7ca1
1manasseh23 <cletavivan@gmail.com>
2023-12-19 15:57:21 -0800

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,6 @@
 """This Write an empty class Square that defines a square
     """
 class Square:
-    """This is a class osf square"""
+    """This is a class of square"""
     pass
+"""This is pass to square
