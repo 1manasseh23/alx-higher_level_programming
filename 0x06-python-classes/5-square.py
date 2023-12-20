2747c34cb0799e96a4fc2f04d21ead879097b149
1manasseh23 <cletavivan@gmail.com>
2023-12-19 17:50:01 -0800

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
+"""
+This Write a class Square that defines
+a square by: (based on 4-square.py)
+"""
+
 
 class Square:
+    """
+    Instantiation with optional
+    Attribute:
+        size
+    """
     def __init__(self, size=0):
         self.size = size
-
+    """
+    property to retrieve
+    Attribute:
+        __size
+    """
     @property
     def size(self):
         return self.__size
-
+    """
+    property setter to set it
+    Attribute:
+        value
+    """
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -16,10 +34,19 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
+    """
+    Public instance method that returns the current square area
+    Attribute:
+        __size
+    """
     def area(self):
         return self.__size ** 2
-
+    """
+    Public instance method that prints in stdout
+    the square with the character #
+    Attribute:
+        __size
+    """
     def my_print(self):
         if self.__size == 0:
             print()
