c9e96deb92b203ac7e13b3cd93aee93b2e0a46c8
1manasseh23 <cletavivan@gmail.com>
2023-12-20 11:05:26 -0800

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
+"""
+This Write a class Square that defines
+a square by: (based on 6-square.py)
+"""
+
 
 class Square:
+    """
+    Instantiation with optional size and optional position
+    Attribute:
+        size: Private instance attribute
+        position: Private instance attribute
+    """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
-
+    """
+    property def size to retrieve
+    Attribute:
+        __size
+    """
     @property
     def size(self):
         return self.__size
-
+    """
+    property setter  to set
+    Attribute:
+        value
+    """
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,11 +36,19 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
+    """
+    property to retrieve
+    Attribute:
+        __position
+    """
     @property
     def position(self):
         return self.__position
-
+    """
+    property setter to set
+    Attribute:
+        value
+    """
     @position.setter
     def position(self, value):
         if (
@@ -33,10 +60,19 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-
+    """
+    Public instance method that returns the current square area
+    Attribute:
+        __size
+    """
     def area(self):
         return self.__size ** 2
-
+    """
+    Public instance method that prints
+    in stdout the square with the character #
+    Attribute:
+        __size
+    """
     def my_print(self):
         if self.__size == 0:
             print()
@@ -45,7 +81,11 @@ class Square:
                 print()
             for _ in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
-
+    """
+    This is a special method it's called on an instance of a class
+    Attribute:
+        __size
+    """
     def __str__(self):
         square_representation = ""
         if self.__size == 0:
