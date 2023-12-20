96053d70f34c435dcd61a314b60ab260d13d1ebd
1manasseh23 <cletavivan@gmail.com>
2023-12-19 16:39:26 -0800

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""This Write an empty class Square that defines a square
-    """
+"""
+This Write an empty class Square that defines a square
+"""
+
 class Square:
-    """This is a class of square
+    """
+    This is a class of square
     Attributes:
         No attrinbute
     """
