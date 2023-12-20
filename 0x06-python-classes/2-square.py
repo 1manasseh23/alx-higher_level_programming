c00db80dd0a6212e2653bbfa43ff95b92bd52a71
1manasseh23 <cletavivan@gmail.com>
2023-12-19 17:13:24 -0800

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+This Write a class Square that defines a
+square by: (based on 1-square.py)
+"""
+
 
 class Square:
+    """
+    This class square
+    Attribute:
+        size
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an inger")
@@ -8,7 +18,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
+    """
+    This is class property
+    Attribute:
+        value
+    """
     @property
     def size(self, value):
         if not isinstance(value, int):
