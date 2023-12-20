bb46c84a3853c9fc40c9c279ac708bf5caecc11f
1manasseh23 <cletavivan@gmail.com>
2023-12-19 17:21:38 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+This Write a class Square that defines
+a square by: (based on 2-square.py)
+"""
+
 
 class Square:
+    """
+    This is a class square
+    Attribute:
+        size
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an inger")
@@ -8,6 +18,10 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
+    """
+    Public instance method that returns the current square area
+    Attribute:
+        __size
+    """
     def area(self):
         return self.__size ** 2
