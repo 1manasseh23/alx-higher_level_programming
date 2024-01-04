b413663d92a49c5665702dc467d15dceb77b4d39
1manasseh23 <cletavivan@gmail.com>
2024-01-04 14:04:09 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Rectangl is an empty class that defines a rectangle
+Rectangl is an empty class that defines
+a rectangle
 """
-
 
 
 class Rectangle:
     """
     This is an empty class
+    Attributes:
+        No attrinbute
     """
     pass
