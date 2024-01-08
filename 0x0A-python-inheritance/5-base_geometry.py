1cdf32c6e672b6375121931dbdf2e60a00483156
1manasseh23 <cletavivan@gmail.com>
2024-01-08 11:15:09 -0800

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    pass
+"""
+This Rectangle is a class that inherits from BaseGeometry
+Attribute:
+    baseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    This is a function that initializes the correspornding
+    attribute of the rectangle object
+    Attribute:
+        width
+        height
+    """
+
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = heigh
