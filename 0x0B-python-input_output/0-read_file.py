f7273f34a178bccac187884c7f35f1ff8181105e
1manasseh23 <cletavivan@gmail.com>
2024-01-09 12:21:18 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,5 +12,5 @@ def read_file(filename=""):
     Read_file its read the contain of a file
     and print it the consol
     """
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
