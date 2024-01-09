9cd0f74c58753ad7986a8405f252b012ba4319c2
1manasseh23 <cletavivan@gmail.com>
2024-01-09 09:52:54 -0800

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Module in Python that provides methods
+for working with JSON data
+"""
+
+
 import json
+
+
 """
 a function that returns the JSON representation
 of an object (string)
