6141ca38f5be804c6837832e341086552f46d3f1
1manasseh23 <cletavivan@gmail.com>
2024-01-09 09:03:16 -0800

    Write to a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-function that writes a string to a text file (UTF8) and
-returns the number of characters written
-Args:
+This is a function that writes a string to a text
+file (UTF8) and returns the number of characters written
+Argument:
     fileename
 """
 
 
 def read_file(filename=""):
+    """
+    Read_file its read the contain of a file
+    and print it to the consol
+    """
     with open(filename, 'r') as f:
         print(f.read())
