f0676ee06ea6a89806653d1b5dd5637e5f6c4083
1manasseh23 <cletavivan@gmail.com>
2024-01-16 09:09:51 -0800

    Base, Rectangle, Square

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,17 +5,78 @@ import unittest
 import warnings
 
 class Testbase(unittest.TestCase):
-    """
-    def setUp(self):
-        self.a = Base()
-        self.b = Base()
 
-    def test_isEquals(self):
+    def test_equal(self):
+        self.assertEqual(1, 1)
+
+    def test_not_equal(self):
+        self.assertNotEqual(1, 2)
+
+    def test_true(self):
+        self.assertTrue(True)
+
+    def test_false(self):
+        self.assertFalse(False)
+
+    def test_is_none(self):
         a = Base()
-        b = Base(12)
-        self.assertEqual(self.a.id, 1)
-        self.assertEqual(self.b.id, 12)
-        """
+        self.assertIsNone(a.id)
+
+    def test_is(self):
+        a = Base()
+        b = a
+        self.assertIs(a, b)
+
+    def test_in(self):
+        a = Base()
+        b = Base(1)
+        self.assertIn(b, a)
+
+    def test_not_in(self):
+        a = Base()
+        b = Base(4)
+        self.assertNotIn(b, a)
+
+    def test_is_instance(self):
+        a = Base()
+        self.assertIsInstance(a, Base)
+
+    def test_not_is_instance(self):
+        a = Base()
+        self.assertNotIsInstance(a, str)
+
+    def test_raises(self):
+        def divide_by_zero():
+            return 1 / 0
+
+        self.assertRaises(ZeroDivisionError, divide_by_zero)
+
+    def test_raises_regex(self):
+        def raise_value_error():
+            raise ValueError("Invalid value")
+
+        self.assertRaisesRegex(ValueError, "Invalid value", raise_value_error)
+
+    def test_warns(self):
+        def warn():
+            warnings.warn("This is a warning message")
+
+        self.assertWarns(Warning, warn)
+
+    def test_almost_equal(self):
+        a = 1.0
+        b = 1.0000001
+        self.assertAlmostEqual(a, b, places=9)
+
+if __name__ == '__main__':
+    unittest.main()
+
+"""
+from models.base import Base
+import unittest
+import warnings
+
+class Testbase(unittest.TestCase):
 
     def test_equal(self):
         self.assertEqual(1, 1)
@@ -90,3 +151,5 @@ class Testbase(unittest.TestCase):
 """
 if __name__ == '__main__':
     unittest.main()
+
+"""
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,78 @@ import unittest
 import warnings
 
 class Testrectangle(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertEqual(1, 1)
+
+    def test_not_equal(self):
+        self.assertNotEqual(1, 2)
+
+    def test_true(self):
+        self.assertTrue(True)
+
+    def test_false(self):
+        self.assertFalse(False)
+
+    def test_is_none(self):
+        a = Rectangle()
+        self.assertIsNone(a.id)
+
+    def test_is(self):
+        a = Rectangle()
+        b = a
+        self.assertIs(a, b)
+
+    def test_in(self):
+        a = Rectangle()
+        b = Rectangle(1)
+        self.assertIn(b, a)
+
+    def test_not_in(self):
+        a = Rectangle()
+        b = Rectangle(4)
+        self.assertNotIn(b, a)
+
+    def test_is_instance(self):
+        a = Rectangle()
+        self.assertIsInstance(a, Rectangle)
+
+    def test_not_is_instance(self):
+        a = Rectangle()
+        self.assertNotIsInstance(a, str)
+
+    def test_raises(self):
+        def divide_by_zero():
+            return 1 / 0
+
+        self.assertRaises(ZeroDivisionError, divide_by_zero)
+
+    def test_raises_regex(self):
+        def raise_value_error():
+            raise ValueError("Invalid value")
+
+        self.assertRaisesRegex(ValueError, "Invalid value", raise_value_error)
+
+    def test_warns(self):
+        def warn():
+            warnings.warn("This is a warning message")
+
+        self.assertWarns(Warning, warn)
+
+    def test_almost_equal(self):
+        a = 1.0
+        b = 1.0000001
+        self.assertAlmostEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
+
+"""
+from models.rectangle import Rectangle
+import unittest
+import warnings
+
+class Testrectangle(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(1, 1)
 
@@ -70,11 +142,7 @@ class Testrectangle(unittest.TestCase):
             warnings.warn("This is a warning message")
 
         self.assertWarns(Warning, warn)
-"""
-    def test_almost_equal(self):
-        a = 1.0
-        b = 1.0000001
-        self.assertAlmostEqual(a, b)
-"""
+
 if __name__ == '__main__':
     unittest.main()
+"""
