5557a2f849e46e885b0e6045c30204a49d57f50b
1manasseh23 <cletavivan@gmail.com>
2024-02-14 11:49:27 -0800

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+ -- 1-create_user.sql
+
+-- Create the user 'user_0d_1' if not already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES
