cf13d5371b15218770f73bf9079b7c26341ce7f2
1manasseh23 <cletavivan@gmail.com>
2024-02-13 19:37:30 -0800

    First table

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,18 @@
+-- 3-list_tables.sql
+
+-- ----------------------------------------------------------
+-- SQL Script: 3-list_tables.sql
+-- Description: This script lists all the tables of a given
+--              database on the MySQL server.
+-- Author: [Your Name]
+-- Date: [Current Date]
+-- ----------------------------------------------------------
+
+-- Parameter: The database name will be passed as an argument
+-- Example Usage: cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
+
+-- List all tables in the specified database
+USE `mysql`;
+
+-- Fetch and display table names
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,24 @@
+-- 4-first_table.sql
+
+-- ----------------------------------------------------------
+-- SQL Script: 4-first_table.sql
+-- Description: This script creates a table named first_table
+--              in the specified database on the MySQL server.
+-- Author: [Your Name]
+-- Date: [Current Date]
+-- ----------------------------------------------------------
+
+-- Parameter: The database name will be passed as an argument
+-- Example Usage: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+-- Create the first_table if it does not already exist
+USE `hbtn_0c_0`;
+
+-- Check if the table exists before creating
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Output a message indicating the operation is complete
+SELECT 'Table first_table created or already exists.' AS 'Status';
