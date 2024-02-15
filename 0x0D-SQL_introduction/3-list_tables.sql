bcdfdbba9d47b51660457ca0ad893d9a730b95b0
1manasseh23 <cletavivan@gmail.com>
2024-02-15 00:48:22 -0800

    List tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,15 +1,4 @@
 -- 3-list_tables.sql
 
--- ----------------------------------------------------------
--- SQL Script: 3-list_tables.sql
--- Description: This script lists all the tables of a given
---              database on the MySQL server.
--- Author: [Your Name]
--- Date: [Current Date]
--- ----------------------------------------------------------
-
--- Parameter: The database name will be passed as an argument
--- Example Usage: cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
-
--- Fetch and display table names
-SHOW TABLES FROM mysql;
+-- Show all tables in the current database
+SHOW FULL TABLES;
