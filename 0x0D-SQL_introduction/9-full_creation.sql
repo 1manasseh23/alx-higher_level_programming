1494d66674b66b4f22c8e464ebb4e31bd308e51a
1manasseh23 <cletavivan@gmail.com>
2024-02-15 15:12:55 -0800

    update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,11 +4,11 @@
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	secore INT NOT NULL
+	score INT NOT NULL
 );
 
-INSERT INT second_table (id, name, score) VALUES
+INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
-(4, 'George', 8);
+(4, 'George', 8)
