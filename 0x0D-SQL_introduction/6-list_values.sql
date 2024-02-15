e4bd9f7a8e6914f757dffd07c50d76298d5e6626
1manasseh23 <cletavivan@gmail.com>
2024-02-15 14:43:31 -0800

    List all in table

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,5 @@
+-- 5-full_table.sql
+
+--  script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 USE hbtn_0c_0;
 SELECT * FROM first_table;
