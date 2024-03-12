eb711fbc323d56e1fe88bc2aac4f0cce2607134e
1manasseh23 <cletavivan@gmail.com>
2024-03-11 20:25:20 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/*
+a script that prints x times “C is fun”
+
+Where x is the first argument of the script
+If the first argument can’t be converted to an integer, print “Missing number of occurrences”
+*/
+
+if (process.argv.length !== 3) {
+  console.log("Missing number of occurrences");
+} else {
+  const x = parseInt(process.argv[2]);
+  if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+  } else {
+    for (let i = 0; i < x; i++) {
+      console.log("C is fun");
+    }
+  }
+}
+
