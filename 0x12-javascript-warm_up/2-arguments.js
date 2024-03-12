e019c6aee11052b207affc782eadb37b40ff679c
1manasseh23 <cletavivan@gmail.com>
2024-03-11 22:36:04 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//  a script that prints a message depending of the number of arguments passed
+/*  a script that prints a message depending of the number of arguments passed
 
 const args = process.argv.slice(2);
 const numArgs = args.length;
@@ -16,4 +16,14 @@ else if (numArgs === 1)
 else
 {
 	console.log("Arguments fund");
+}
+*/
+const argsLength = process.argv.length;
+
+if (argsLength === 2) {
+    console.log("No argument");
+} else if (argsLength === 3) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
 }
