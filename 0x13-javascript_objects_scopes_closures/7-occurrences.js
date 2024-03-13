580e28dad595a40f0d9639d3326859742bd4623c
1manasseh23 <cletavivan@gmail.com>
2024-03-12 19:21:24 -0700

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+//  a function that returns the number of occurrences in a list
+
+exports.nbOccurences = function(list, searchElement) {
+    let count = 0;
+    for (let element of list) {
+        if (element === searchElement) {
+            count++;
+        }
+    }
+    return count;
+};
