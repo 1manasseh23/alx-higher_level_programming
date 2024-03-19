65c585f38bf8c291610991a2585182ff61f00ecf
1manasseh23 <cletavivan@gmail.com>
2024-03-19 04:55:40 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,12 +8,17 @@ from model_state import Base, State
 if __name__ == "__main__":
     # Check if all required arguments are provided
     if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <mysql_username> <mysql_password> <database_name>")
+        print(
+                f"Usage: {sys.argv[0]} <mysql_username> \
+                        <mysql_password> <database_name>"
+                        )
         sys.exit(1)
 
     # Create an engine to connect to the MySQL server
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+            )
 
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
