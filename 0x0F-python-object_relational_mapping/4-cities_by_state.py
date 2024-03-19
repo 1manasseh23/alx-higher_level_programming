5511a1df4d048d18b784f35056ee080e22ce875e
1manasseh23 <cletavivan@gmail.com>
2024-03-19 04:41:35 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,9 +21,11 @@ if __name__ == "__main__":
         cursor = db.cursor()
 
         # Execute query
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+        cursor.execute(
+                "SELECT cities.id, cities.name, states.name FROM cities \
                         JOIN states ON cities.state_id = states.id \
-                        ORDER BY cities.id ASC")
+                        ORDER BY cities.id ASC"
+                        )
         results = cursor.fetchall()
 
         # Display results
