03d01e67063532db8fe629954bcf2eb3e2731e31
1manasseh23 <cletavivan@gmail.com>
2024-03-19 07:32:01 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,44 +13,31 @@ of 128 characters and can’t be null
 class attribute state_id that represents a column of an integer,
 can’t be null and is a foreign key to states.id
 """
-from model_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State, City
-
-
-def fetch_cities_by_state(username, password, db_name):
-    # Create a database connection
-    engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
-            )
-
-    # Bind the engine to the Base
-    Base.metadata.create_all(engine)
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    try:
-        # Query all City objects and sort by id
-        cities = session.query(City).order_by(City.id).all()
-
-        # Print results in the specified format
-        for city in cities:
-            print(f"{city.state.name}: ({city.id}) {city.name}")
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    finally:
-        session.close()
+from model_state import Base, State
+from model_city import City
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py <username> \
-                <password> <db_name>")
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    fetch_cities_by_state(username, password, db_name)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                username,
+                password,
+                db_name),
+            pool_pre_ping=True
+            )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(
+            City, State).join(State).order_by(City.id).all()
+
+    for city, state in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,16 +10,12 @@ class attribute id that represents a column of an auto-generated,
 unique integer, can’t be null and is a primary key
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
-
-Base = declarative_base()
 
 
 class City(Base):
+    """Class definition of a City"""
     __tablename__ = 'cities'
-    id = Column(
-            Integer, primary_key=True, nullable=False, autoincrement=True
-            )
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
