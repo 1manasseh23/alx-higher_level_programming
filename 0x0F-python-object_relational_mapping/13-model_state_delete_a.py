7bb94401c1d340979b492ad454c4f33f14c6375a
1manasseh23 <cletavivan@gmail.com>
2024-03-19 06:55:56 -0700

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-
-# 13-model_state_delete_a.py
-
+"""
+This a script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def delete_states_with_a(username, password, db_name):
     # Create a database connection
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+            )
 
     # Bind the engine to the Base
     Base.metadata.create_all(engine)
@@ -19,23 +23,27 @@ def delete_states_with_a(username, password, db_name):
 
     try:
         # Query and delete State objects with 'a' in their name
-        states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+        states_to_delete = session.query(State).filter(
+                State.name.like('%a%')).all()
         for state in states_to_delete:
             session.delete(state)
 
         # Commit the changes
         session.commit()
-        print("States with 'a' in their name have been deleted successfully.")
+        print("States with 'a' in their name \
+                have been deleted successfully.")
     except Exception as e:
         print(f"Error occurred: {e}")
         session.rollback()
     finally:
         session.close()
 
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 4:
-        print("Usage: ./13-model_state_delete_a.py <username> <password> <db_name>")
+        print("Usage: ./13-model_state_delete_a.py <username> \
+                <password> <db_name>")
         sys.exit(1)
 
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
