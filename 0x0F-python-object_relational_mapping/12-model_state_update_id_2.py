82b850722b74208388a907a08f86394f2322d1fc
1manasseh23 <cletavivan@gmail.com>
2024-03-19 06:47:26 -0700

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-
-# 12-model_state_update_id_2.py
-
+"""
+This a script that changes the name of a State
+object from the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     # Check if all required arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <mysql_username> <mysql_password> \
+                <database_name>".format(sys.argv[0]))
         sys.exit(1)
 
     # Create an engine to connect to the MySQL server
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, db_name))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, db_name))
 
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
