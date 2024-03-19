63c17c9d6c70d2f29661dadb484b3acc00d30b35
1manasseh23 <cletavivan@gmail.com>
2024-03-19 07:06:56 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
+"""
+This a Python file similar to model_state.py named model_city.py
+that contains the class definition of a City.
 
+City class:
+inherits from Base (imported from model_state)
+links to the MySQL table cities
+class attribute id that represents a column of an auto-generated,
+unique integer, can’t be null and is a primary key
+class attribute name that represents a column of a string
+of 128 characters and can’t be null
+class attribute state_id that represents a column of an integer,
+can’t be null and is a foreign key to states.id
+"""
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State, City
 
+
 def fetch_cities_by_state(username, password, db_name):
     # Create a database connection
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+            )
 
     # Bind the engine to the Base
     Base.metadata.create_all(engine)
@@ -28,10 +44,12 @@ def fetch_cities_by_state(username, password, db_name):
     finally:
         session.close()
 
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py <username> <password> <db_name>")
+        print("Usage: ./14-model_city_fetch_by_state.py <username> \
+                <password> <db_name>")
         sys.exit(1)
 
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+"""
+This a Python file similar to model_state.py named model_city.py
+that contains the class definition of a City.
 
-# model_city.py
-
+City class:
+inherits from Base (imported from model_state)
+links to the MySQL table cities
+class attribute id that represents a column of an auto-generated,
+unique integer, can’t be null and is a primary key
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 Base = declarative_base()
 
+
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(
+            Integer, primary_key=True, nullable=False, autoincrement=True
+            )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
