176c68305c91684c8b3cc2e0651ffa2ba5df0a36
1manasseh23 <cletavivan@gmail.com>
2024-04-23 16:44:43 +0100

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+/*
+* This a script that gets the contents of a webpage and stores it in a file.
+
+* The first argument is the URL to request
+The second argument the file path to store the body response
+* The file must be UTF-8 encoded
+*  You must use the module request
+*/
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      return;
+    }
+
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+  });
+});
