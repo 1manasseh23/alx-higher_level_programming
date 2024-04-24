9b3d264815adadad7f69a687d8b12b075358aa55
1manasseh23 <cletavivan@gmail.com>
2024-04-23 19:23:59 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,13 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 /*
 * This a script that prints all characters of a Star Wars movie:
-
 * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
 * Display one character name by line
 * You must use the Star wars API
 * You must use the module request
 */
+
+
 const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
