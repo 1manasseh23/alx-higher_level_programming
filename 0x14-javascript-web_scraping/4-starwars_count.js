881debe6352e82f4fc2c9c00c2ef9cdcbf895f16
1manasseh23 <cletavivan@gmail.com>
2024-04-23 19:08:53 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 /*
 * This a script that prints the number of movies where the character “Wedge Antilles” is present.
 
@@ -6,6 +6,7 @@
 *  Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
 * You must use the module request
 */
+
 const request = require('request');
 const url = process.argv[2];
 const characterId = 18;
