761257ed138fe67db6804e7d172a39d5547213c6
1manasseh23 <cletavivan@gmail.com>
2024-04-24 02:22:13 +0100

    Status code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,8 @@
 #!/usr/bin/env node
-/**
- * Writes a string to a file.
- *
- * @param {string} filePath - The file path to write to.
- * @param {string} stringToWrite - The string to write to the file.
- * @throws {Error} If an error occurred during while writing, the error object is thrown.
- */
 
 const fs = require('fs');
 
 function writeToFile(filePath, stringToWrite) {
-  // Write the string to the file in UTF-8 encoding
   fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
     if (err) {
       console.error(err);
@@ -20,5 +12,4 @@ function writeToFile(filePath, stringToWrite) {
   });
 }
 
-// Call the function with the file path and string to write as arguments
 writeToFile(process.argv[2], process.argv[3]);
