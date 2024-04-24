31caca6267b55d9b65f0a15748b533c09d842d67
1manasseh23 <cletavivan@gmail.com>
2024-04-23 19:36:29 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,8 +6,6 @@
 * You must use the Star wars API
 * You must use the module request
 */
-
-
 const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
