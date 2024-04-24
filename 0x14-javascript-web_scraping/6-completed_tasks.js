60673835b782898a380787043163b5efac1d82b3
1manasseh23 <cletavivan@gmail.com>
2024-04-23 19:50:04 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 /*
-* This a script that computes the number of tasks
-* completed by user id.
-* The first argument is the API
-* URL: https://jsonplaceholder.typicode.com/todos
+* This a script that computes the number of tasks completed by user id.
+* The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
 * Only print users with completed task
 * You must use the module request
 */
@@ -35,6 +33,6 @@ request(apiUrl, (error, response, body) => {
     }
   });
 
+
   console.log(completedTasksByUserId);
 });
-
