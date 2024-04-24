1c8b6672d7de92584d55bb04828fb328abdaa2ec
1manasseh23 <cletavivan@gmail.com>
2024-04-23 18:49:19 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,29 +1,15 @@
 #!/usr/bin/node
-/*
- * This a script that writes a string to a file.
-
- * The first argument is the file path
- * The second argument is the string to write
- * The content of the file must be written in utf-8
- * If an error occurred during while writing, print the error object
- */
 
 const fs = require('fs');
 
-function writeToFile(filePath, stringToWrite)
-{
-	fs.writeFile(filePath, stringToWrite, 'utf-8', (err) =>
-		{
-			if (err)
-			{
-				console.error(err);
-			}
-			else
-			{
-				console.log(`Successfully written to ${filePath}`);
-			}
-		});
+function writeToFile(filePath, stringToWrite) {
+  fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Successfully written to ${filePath}`);
+    }
+  });
 }
 
 writeToFile(process.argv[2], process.argv[3]);
-
