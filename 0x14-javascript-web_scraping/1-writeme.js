acace19edde56a64b119e6b68533e6e5c598c6b3
1manasseh23 <cletavivan@gmail.com>
2024-04-23 18:43:15 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -11,13 +11,13 @@
 const fs = require('fs');
 
 function writeToFile(filePath, stringToWrite) {
-  fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Successfully written to ${filePath}`);
-    }
-  });
+	fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(`Successfully written to ${filePath}`);
+		}
+	});
 }
 
 writeToFile(process.argv[2], process.argv[3]);
