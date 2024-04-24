d11ec5fdb8e5ad324d04802fc1f1989e48d79af5
1manasseh23 <cletavivan@gmail.com>
2024-04-23 19:34:59 -0700

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,8 +7,6 @@
 * You must use the Star wars API
 * You must use the module request
 */
-
-
 const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
