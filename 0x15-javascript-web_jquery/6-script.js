f3bd7fc6b0c6219deaeabdaa9abf12d7d8ea996c
1manasseh23 <cletavivan@gmail.com>
2024-05-02 20:58:51 +0100

    Change the text

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,4 +1,6 @@
+// Wait for the document to be ready
 $(document).ready(function() {
+  //  To update the hearder  web click
   $('#update_header').click(function() {
     $('header').text('New Header!!!');
   });
